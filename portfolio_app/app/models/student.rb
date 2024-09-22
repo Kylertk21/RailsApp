dbe815f2437fc3373a208dc028cd06228603b956
kyler <kyler@kramer.kramer>
2024-09-22 13:40:11 -0600

    completed tests, implemented email format and server-side form lengths

--- portfolio_app/app/models/student.rb
+++ portfolio_app/app/models/student.rb
@@ -1,8 +1,12 @@
 class Student < ApplicationRecord
-  validates:school_email, presence:true, uniqueness:true
-  validates:first_name, presence:true
-  validates:last_name, presence:true
-  validates:grad_date, presence:true
-  validates:major, presence:true
+  validates:first_name, presence:true, length: {maximum: 100}
+  validates:last_name, presence:true, length: {maximum: 100}
+  validates:grad_date, presence:true, length: {maximum: 100}
+  validates:major, presence:true, length: {maximum: 255}
   has_one_attached :image
+  validates :school_email, presence:true, length: {maximum: 255}, uniqueness:true, format: { 
+    with: /\A[\w+\-.]+@msudenver\.edu\z/i,
+    message: "must be a valid MSU Denver email address"
+  }
 end
+
--- portfolio_app/test/models/student_test.rb
+++ portfolio_app/test/models/student_test.rb
@@ -3,11 +3,40 @@ require "test_helper"
 class StudentTest < ActiveSupport::TestCase
   include ActiveStorage::Blob::Analyzable
   fixtures :students
+  
   test "successfully insert student to db" do
     student = students(:one)
     assert student.valid?
     assert_not_nil Student.find_by(school_email:"Test@msudenver.edu")
+  end
+
+  test "required fields notpresent" do
+    student = students(:blank)
+    assert_not student.valid?
+    assert_includes student.errors[:last_name], "can't be blank"
+    assert_includes student.errors[:school_email], "can't be blank"
+    assert_includes student.errors[:major], "can't be blank"
+  end
+  
+  test "max length of fields" do
+    student = students(:max)
+    assert_not student.valid?
+    assert_includes student.errors[:last_name], "is too long (maximum is 100 characters)"
+  end
+
+  test "updated student information" do
+    student = students(:one)
+    assert student.update(last_name: "Updated", major: "Math")
+    assert_equal "Updated", student.reload.last_name
+    assert_equal "Math", student.reload.major
+  end
+
+  test "delete student" do
+    student = students(:one)
+    assert_difference 'Student.count', -1 do
+      student.destroy
     end
+  end
 
   test "duplicate email error" do
     assert_raises ActiveRecord::RecordInvalid do
@@ -15,12 +44,18 @@ class StudentTest < ActiveSupport::TestCase
     end
   end
   
-  test "unentered form data error" do
-    assert_raises ActiveRecord::RecordInvalid do
-      Student.create!(last_name:"", school_email: "", major:"")
-    end
+  test "valid email address" do
+    student = students(:one)
+    assert student.valid?
   end
 
+  test "invalid email format" do
+    student = students(:two)
+    assert_not student.valid?
+    assert_includes student.errors[:school_email], "must be a valid MSU Denver email address"
+  end
+
+  
   test "successfully upload profile picture" do
     student = students(:one)
     file = fixture_file_upload(Rails.root.join('test', 'fixtures', 'files', 'profile.png'), 'image/png')
