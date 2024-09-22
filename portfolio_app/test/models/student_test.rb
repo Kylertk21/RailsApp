0d83e8d1f9631397a463a66d8a9d951224ddc3c1
kyler <kyler@kramer.kramer>
2024-09-22 01:08:14 -0600

    added tests

--- portfolio_app/test/models/student_test.rb
+++ portfolio_app/test/models/student_test.rb
@@ -1,6 +1,13 @@
 require "test_helper"
 
 class StudentTest < ActiveSupport::TestCase
+  fixtures :students
+  test "successfully insert student to db" do
+    test = students(:one)
+    assert test.valid?
+    assert_not_nil Student.find_by(school_email:"Test@msudenver.edu")
+    end
+
   test "duplicate email error" do
     assert_raises ActiveRecord::RecordInvalid do
       Student.create!(last_name: "Test", school_email: "Test@msudenver.edu", major: "CS")
@@ -12,7 +19,4 @@ class StudentTest < ActiveSupport::TestCase
       Student.create!(last_name:"", school_email: "", major:"")
     end
   end
-
-
-
-end
+end 
