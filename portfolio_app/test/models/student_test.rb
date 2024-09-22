c45b7c61897addb70142a43fa06ce620117fab60
kyler <kyler@kramer.kramer>
2024-09-22 03:06:28 -0600

    added test for picture upload

--- portfolio_app/test/models/student_test.rb
+++ portfolio_app/test/models/student_test.rb
@@ -1,10 +1,11 @@
 require "test_helper"
 
 class StudentTest < ActiveSupport::TestCase
+  include ActiveStorage::Blob::Analyzable
   fixtures :students
   test "successfully insert student to db" do
-    test = students(:one)
-    assert test.valid?
+    student = students(:one)
+    assert student.valid?
     assert_not_nil Student.find_by(school_email:"Test@msudenver.edu")
     end
 
@@ -19,4 +20,13 @@ class StudentTest < ActiveSupport::TestCase
       Student.create!(last_name:"", school_email: "", major:"")
     end
   end
-end 
+
+  test "successfully upload profile picture" do
+    student = students(:one)
+    file = fixture_file_upload(Rails.root.join('test', 'fixtures', 'files', 'profile.png'), 'image/png')
+    student.image.attach(file)
+    assert student.image.attached?
+    assert_equal "image/png", student.image.content_type
+    assert student.image.byte_size > 0
+    end
+end
