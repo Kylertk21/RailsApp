0e468ec3e9cb883ccb66eeee30c8ff6eb7b2c5fe
kyler <kyler@kramer.kramer>
2024-09-22 02:36:55 -0600

    upload photo via active directory, displays on student profile

--- portfolio_app/app/models/student.rb
+++ portfolio_app/app/models/student.rb
@@ -1,8 +1,8 @@
 class Student < ApplicationRecord
-  # Ensures school email is valid and unique
   validates:school_email, presence:true, uniqueness:true
   validates:first_name, presence:true
   validates:last_name, presence:true
   validates:grad_date, presence:true
   validates:major, presence:true
+  has_one_attached :image
 end
