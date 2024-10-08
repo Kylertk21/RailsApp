8eb47b5ba30dc6476f77106b306b4e930b58134d
kyler <kyler@kramer.kramer>
2024-10-08 00:59:12 -0600

    fixed drop down feature

--- portfolio_app/app/models/student.rb
+++ portfolio_app/app/models/student.rb
@@ -2,14 +2,14 @@ class Student < ApplicationRecord
   validates:first_name, presence:true, length: {maximum: 100}
   validates:last_name, presence:true, length: {maximum: 100}
   validates:grad_date, presence:true, length: {maximum: 100}
-  validates:major, presence:true, length: {maximum: 255}, inclusion: { in VALID_MAJORS, message: "%{value} is not a valid major}
+  
+VALID_MAJORS = ["Computer Engineering BS", "Computer Information Systems BS",
+                  "Computer Science BS, Cybersecurity Major", "Data Science and Machine Learning Major"]
+  validates:major, presence:true, length: {maximum: 255}, inclusion: { in: VALID_MAJORS, message: "%{value} is not a valid major"}
   has_one_attached :image
   validates :school_email, presence:true, length: {maximum: 255}, uniqueness:true, format: { 
     with: /\A[\w+\-.]+@msudenver\.edu\z/i,
     message: "must be a valid MSU Denver email address"
   }
-  VALID_MAJORS = ["Computer Engineering BS", "Computer Information Systems BS",
-                  "Computer Science BS, Cybersecurity Major", "Data Science and Machine Learning Major"]
-
 end
 
