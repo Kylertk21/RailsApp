3a9899c78c3ecef4a47ea8dc075ad602baa88296
kyler <kyler@kramer.kramer>
2024-10-21 00:15:31 -0600

    finished UI working on rspec

--- portfolio_app/db/seeds.rb
+++ portfolio_app/db/seeds.rb
@@ -1,15 +1,24 @@
 require 'faker'
+require 'open-uri'
 
-Student.delete_all
+Student.find_each do |student|
+  student.image.purge if student.image.attached?
+end
 
-10.times do
-  student = Student.new(
+ActiveStorage::Blob.where.missing(:attachments).find_each(&:purge)
+
+25.times do |i|
+  student = Student.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     school_email: "#{Faker::Internet.username(specifier: "#{Faker::Name.first_name.downcase}.#{Faker::Name.last_name.downcase}")}@msudenver.edu",
     grad_date: Faker::Date.between(from: '2023-01-01', to: '2024-12-30'),
     major: Student::VALID_MAJORS.sample
   )
+
+  image_url = "https://robohash.org/#{student.first_name.gsub(' ', '')}"
+  image = URI.open(image_url)
+  student.image.attach(io: image, filename: "#{student.first_name}.jpg")
 
   if student.save
     puts "Created: #{student.first_name} #{student.last_name}"
