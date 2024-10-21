2c1e278c90ee45e33ae1ba48772f67f8fe3e07ee
kyler <kyler@kyler.kyler>
2024-10-21 13:23:16 -0600

    almost finished with rspec

--- portfolio_app/spec/students_spec.rb
+++ portfolio_app/spec/students_spec.rb
@@ -86,8 +86,9 @@ RSpec.describe "Students", type: :request do
         expect { 
           post students_path, params: { student: valid_student }
         }.to change(Student, :count).by(1)
-        expect(response).to have_http_status(:created) # 201 
-        expect(response.body).to include("Student was succesfully created")
+        expect(response).to have_http_status(:found) # App redirects after student creation, this verifies that the student is created
+        follow_redirect!
+        expect(response.body).to include("Student was successfully created")
       end
 
     end
@@ -95,6 +96,21 @@ RSpec.describe "Students", type: :request do
     context "with invalid parameters" do
       # Test 9 (Student will complete this part)
       # Ensure it does not create a student and returns a 422 status
+      it "does not create student and returns 422" do 
+      invalid_student = { 
+        first_name: "", 
+        last_name: "", 
+        school_email: "", 
+        major: "", 
+        grad_date: "" 
+      }
+
+      expect { 
+      post students_path, params: { student:invalid_student } }.to_not change(Student, :count)
+
+    expect(response).to have_http_status(422) 
+    expect(response.body).to include("7 errors prohibited this student from being saved:")
+      end
     end
   end
 
@@ -105,13 +121,30 @@ RSpec.describe "Students", type: :request do
 
       # Test 10 (Student will complete this part)
       # Ensure it returns a successful response (200 OK)
+    it "returns a successful response (200 OK)" do
+      get student_path(student)
+      expect(response).to have_http_status(200)
+    end
 
       # Test 11 (Student will complete this part)
       # Ensure it includes the student's details in the response body
+    it "includes student's details in response body" do
+      get student_path(student)
+      expect(response.body).to include("Aaron")
+      expect(response.body).to include("Gordon")
+      expect(response.body).to include("gordon@msudenver.edu")
+      expect(response.body).to include("Computer Science BS")
+      expect(response.body).to include("2025-05-15")
     end
+  end
 
     # Test 12: Handle missing records
-
+    context "when student doesn't exist" do
+      it "returns a 404 status" do
+        get "/students/9999"
+      expect(response).to have_http_status(404)
+      end
+    end
   end
 
   # DELETE /students/:id (destroy)
@@ -119,12 +152,21 @@ RSpec.describe "Students", type: :request do
     let!(:student) { Student.create!(first_name: "Aaron", last_name: "Gordon", school_email: "gordon@msudenver.edu", major: "Computer Science BS", grad_date: "2025-05-15") }
 
     # Test 13: Deletes the student and redirects
+    it "deletes the student and redirects" do 
+      expect { 
+        delete student_path(student) }.to change(Student, :count).by(-1)
+
+    expect(response).to have_http_status(:found)
+    follow_redirect!
+    expect(response.body).to include("Student was successfully deleted")
+    expect(Student.exists?(student.id)).to be_falsey
 
 
     # Test 14: Returns a 404 when trying to delete a non-existent student
     it "returns a 404 status when trying to delete a non-existent student" do
       delete "/students/9999"
       expect(response).to have_http_status(:not_found)
+      end
     end
   end
 end
