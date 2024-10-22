d5b7eedc795e01e1ddbcf742b8d7e68df4117df4
kyler <kyler@kyler.kyler>
2024-10-22 12:55:05 -0600

    devise pad implemented

--- portfolio_app/app/models/student.rb
+++ portfolio_app/app/models/student.rb
@@ -1,4 +1,8 @@
 class Student < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   scope :by_major, ->(major) { where(major: major) if major.present? }
   validates:first_name, presence:true, length: {maximum: 100}
   validates:last_name, presence:true, length: {maximum: 100}
